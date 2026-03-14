76b03fd6fd63b4ac718ec126e527a64729231e57
CaseChips <ncasemasana04@gmail.com>
2026-03-14 14:43:44 +0100

    More tools + horse grooming

--- HorseRanch/Assets/Scripts/ManurePile.cs
+++ HorseRanch/Assets/Scripts/ManurePile.cs
@@ -5,13 +5,21 @@ public class ManurePile : MonoBehaviour, IInteractable
     public HorseStats linkedHorse;
     public void Interact()
     {
-        Debug.Log("You cleaned up the manure pile.");
+        ItemData currentItem = InventoryManager.instance.GetActiveItem();
 
-        Destroy(gameObject);
-
-        if(linkedHorse != null)
+        if (currentItem != null && currentItem.itemName == "Pitchfork")
         {
-            linkedHorse.IncreaseStat("hygiene", 25f);
+            if (linkedHorse != null)
+            {
+                linkedHorse.IncreaseStat("hygiene", 20f);
+            }
+
+            Debug.Log("Cleaned the stall with the Pitchfork!");
+            Destroy(gameObject); 
+        }
+        else
+        {
+            Debug.Log("Gross! You need to equip the Pitchfork to clean this up.");
         }
     }
 }
